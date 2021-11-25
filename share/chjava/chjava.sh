0af28d0d7aa3cab9c1f04e2c742ccb91518f62b7
Matt Patterson <matt@reprocessed.org>
2021-11-25 15:59:05 +0100

    Update chjava.sh to fix shellcheck warnings

--- share/chjava/chjava.sh
+++ share/chjava/chjava.sh
@@ -2,28 +2,31 @@ CHJAVA_VERSION="0.0.1"
 CHJAVA_LIBRARY_JDK_DIR="/Library/Java/JavaVirtualMachines"
 CHJAVA_JDKS=()
 
+function chjava_jdk_entry() {
+  local jdk_arch jdk_version
+  jdk_arch=$(file "${CHJAVA_LIBRARY_JDK_DIR}/$1/Contents/MacOS/libjli.dylib" | rev | cut -d ' ' -f 1 | rev)
+  jdk_version=$(/usr/libexec/PlistBuddy -c "print JavaVM:JVMVersion" "${CHJAVA_LIBRARY_JDK_DIR}/$1/Contents/Info.plist")
+  echo "$1:$jdk_version:$jdk_arch"
+}
+
 # /usr/libexec/java_home seems buggy in macOS 11 / 12, so we have to
 # handroll the list
-for jdk in $(ls ${CHJAVA_LIBRARY_JDK_DIR}); do
-  local jdk_arch=$(file "${CHJAVA_LIBRARY_JDK_DIR}/$jdk/Contents/MacOS/libjli.dylib" | rev | cut -d ' ' -f 1 | rev)
-  local jdk_version=$(/usr/libexec/PlistBuddy -c "print JavaVM:JVMVersion" "${CHJAVA_LIBRARY_JDK_DIR}/${jdk}/Contents/Info.plist")
-  CHJAVA_JDKS+="$jdk:$jdk_version:$jdk_arch"
+for jdk in "${CHJAVA_LIBRARY_JDK_DIR}"/*.jdk; do
+  CHJAVA_JDKS+=("$(chjava_jdk_entry "$jdk")}")
 done
 unset jdk
 
-function chjava_reset()
-{
-	[[ -z "$JAVA_HOME" ]] && return
+function chjava_reset() {
+  [[ -z "$JAVA_HOME" ]] && return
 
-	unset JAVA_HOME
-	hash -r
+  unset JAVA_HOME
+  hash -r
 }
 
-function chjava_use()
-{
-	export JAVA_HOME="$1"
+function chjava_use() {
+  export JAVA_HOME="$1"
   echo "$1"
-	hash -r
+  hash -r
 }
 
 function chjava_jdk_name() {
@@ -38,31 +41,29 @@ function chjava_jdk_arch() {
   echo "$1" | cut -d ':' -f 3
 }
 
-function chjava_list_arch()
-{
+function chjava_list_arch() {
   local jdk name
   for jdk in "${CHJAVA_JDKS[@]}"; do
     name=$(chjava_jdk_name "$jdk")
     if [[ "${CHJAVA_LIBRARY_JDK_DIR}/${name}" == "$JAVA_HOME" ]]; then
-      echo " * ${name} $(chjava_jdk_version $jdk) [$(chjava_jdk_arch $jdk)]"
+      echo " * ${name} $(chjava_jdk_version "$jdk") [$(chjava_jdk_arch "$jdk")]"
     else
-      echo "   ${name} $(chjava_jdk_version $jdk) [$(chjava_jdk_arch $jdk)]"
+      echo "   ${name} $(chjava_jdk_version "$jdk") [$(chjava_jdk_arch "$jdk")]"
     fi
   done
 }
 
-function chjava_match()
-{
+function chjava_match() {
   # request="$1"
   # requset_arch=$2
-  local candidates candidate_versions version match
+  local candidates version match
   candidates=()
 
   for jdk in "${CHJAVA_JDKS[@]}"; do
-    version="$(chjava_jdk_version $jdk)"
+    version="$(chjava_jdk_version "$jdk")"
 
-    if echo "$version" | egrep "^$1" > /dev/null && [[ "$(chjava_jdk_arch $jdk)" == "$2" ]]; then
-      candidates+="$jdk"
+    if echo "$version" | grep -E "^$1" > /dev/null && [[ "$(chjava_jdk_arch "$jdk")" == "$2" ]]; then
+      candidates+=("$jdk")
     fi
   done
 
@@ -75,20 +76,19 @@ function chjava_match()
   chjava_jdk_name "${match}"
 }
 
-function chjava()
-{
-	case "$1" in
-		-h|--help)
-			echo "usage: chjava [VERSION|system] [ARCH]"
-			;;
-		-V|--version)
-			echo "chjava: $CHJAVA_VERSION"
-			;;
-		"")
+function chjava() {
+  case "$1" in
+    -h|--help)
+      echo "usage: chjava [VERSION|system] [ARCH]"
+      ;;
+    -V|--version)
+      echo "chjava: $CHJAVA_VERSION"
+      ;;
+    "")
       chjava_list_arch
-			;;
-		system) chjava_reset ;;
-		*)
+      ;;
+    system) chjava_reset ;;
+    *)
       local default_arch arch match
       default_arch="$(uname -m)"
       if [[ -z $2 ]]; then
@@ -99,13 +99,13 @@ function chjava()
 
       match=$(chjava_match "$1" "$arch")
 
-			if [[ -z "$match" ]]; then
-				echo "chjava: unknown Java: $1" >&2
-				return 1
-			fi
+      if [[ -z "$match" ]]; then
+        echo "chjava: unknown Java: $1" >&2
+        return 1
+      fi
 
-			shift
-			chjava_use "${CHJAVA_LIBRARY_JDK_DIR}/${match}"
-			;;
-	esac
+      shift
+      chjava_use "${CHJAVA_LIBRARY_JDK_DIR}/${match}"
+      ;;
+  esac
 }
