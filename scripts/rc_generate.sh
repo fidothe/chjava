93a427d4a7f5ba4b523d1e4ca8d5273ae4dbf838
Matt Patterson <matt@reprocessed.org>
2021-11-29 14:12:15 +0100

    Implement auto-java-version-setting
    Obviously, nicked wholesale from chruby, with changes where needed to
    support the things chjava does differently.

--- scripts/rc_generate.sh
+++ scripts/rc_generate.sh
@@ -11,6 +11,7 @@ export PREFIX="${PREFIX:-/usr/local}"
 config="if [ -d "$PREFIX/share/chjava" ]; then
   if [ -n \"\$BASH_VERSION\" ] || [ -n \"\$ZSH_VERSION\" ]; then
     source \"$PREFIX/share/chjava/chjava.sh\"
+    source \"$PREFIX/share/chjava/auto.sh\"
   fi
 fi"
 
--- test/unit/chjava_test.sh
+++ test/unit/chjava_test.sh
@@ -2,7 +2,8 @@
 
 function setUp()
 {
-  test_jdks=("${test_jvm_library_dir}/emperor-11.jdk:emperor-11.jdk:11.0.1:${test_default_arch}" "${test_jvm_library_dir}/rockhopper-11.jdk:rockhopper-11.jdk:11.0.8:${test_default_arch}")
+  test_jdks=("$(mock_jdk emperor-11.jdk 11.0.1 ${test_default_arch})"\
+    "$(mock_jdk rockhopper-11.jdk 11.0.8 ${test_default_arch})")
   original_jdks=(${CHJAVA_JDKS[@]})
   original_library_jdk_dir="${CHJAVA_LIBRARY_JDK_DIR}"
   CHJAVA_LIBRARY_JDK_DIR="${test_jvm_library_dir}"
@@ -10,49 +11,43 @@ function setUp()
   expected_java_home="${test_jvm_library_dir}/rockhopper-11.jdk/Contents/Home"
 }
 
-function test_chjava_X()
-{
+function test_chjava_X() {
   CHJAVA_JDKS=(${test_jdks[@]})
   chjava "$test_java_version_x" >/dev/null
 
   assertEquals "did not match $test_java_version_x" "${expected_java_home}" "$JAVA_HOME"
 }
 
-function test_chjava_1_does_not_match_11()
-{
+function test_chjava_1_does_not_match_11() {
   CHJAVA_JDKS=(${test_jdks[@]})
   chjava "1" 2>/dev/null
 
   assertEquals "version '1' matched version '11'" 1 $?
 }
 
-function test_chjava_11_0_1_matches_11_0_1_123()
-{
-  CHJAVA_JDKS=("$(chjava_mock_jdk_entry humboldt-11.jdk 11.0.1_123 ${test_default_arch})")
+function test_chjava_11_0_1_matches_11_0_1_123() {
+  CHJAVA_JDKS=("$(mock_jdk humboldt-11.jdk 11.0.1_123 ${test_default_arch})")
   chjava "11" 2>/dev/null
   expected_java_home="${test_jvm_library_dir}/humboldt-11.jdk/Contents/Home"
 
   assertEquals "did not match '11.0.1_123'" "${expected_java_home}" "$JAVA_HOME"
 }
 
-function test_chjava_X_plus_arch()
-{
+function test_chjava_X_plus_arch() {
   CHJAVA_JDKS=(${test_jdks[@]})
   chjava "$test_java_version_x" ${test_default_arch} >/dev/null
 
   assertEquals "did not match $test_java_version_x" "${expected_java_home}" "$JAVA_HOME"
 }
 
-function test_chjava_X_is_name()
-{
+function test_chjava_X_is_name() {
   CHJAVA_JDKS=(${test_jdks[@]})
   chjava "rockhopper-11.jdk" >/dev/null
 
   assertEquals "did not match $test_java_version_x" "${expected_java_home}" "$JAVA_HOME"
 }
 
-function test_chjava_exact_match_first()
-{
+function test_chjava_exact_match_first() {
   CHJAVA_JDKS=(${test_jdks[@]})
 
   chjava "11.0.1"
@@ -60,8 +55,7 @@ function test_chjava_exact_match_first()
   assertEquals "did not use the exact match" "${test_jvm_library_dir}/emperor-11.jdk/Contents/Home" "$JAVA_HOME"
 }
 
-function test_chjava_system()
-{
+function test_chjava_system() {
   CHJAVA_JDKS=(${test_jdks[@]})
 
   chjava "$test_java_version" >/dev/null
@@ -70,15 +64,13 @@ function test_chjava_system()
   assertNull "did not reset JAVA_HOME" "$JAVA_HOME"
 }
 
-function test_chruby_unknown()
-{
+function test_chruby_unknown() {
   chjava "does_not_exist" 2>/dev/null
 
   assertEquals "did not return 1" 1 $?
 }
 
-function tearDown()
-{
+function tearDown() {
   chjava_reset
 
   CHJAVA_JDKS=(${original_jdks[@]})
